5e0ebb166f66103ca1f09a472d16824503e1c745
h64 <hhongnr@gmail.com>
2019-09-26 16:58:36 -0700

    form work

--- blueprints/characters.py
+++ blueprints/characters.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, redirect, render_template, request
+from datetime import datetime
 
 character_blueprint = Blueprint('characters', __name__, url_prefix='/characters')
 
@@ -7,4 +8,13 @@ def index():
     if request.method == 'GET':
         return render_template('characters/index.html')
     elif request.method == 'POST':
-        return 'POST REACHED'
+        name = request.form['name']
+        # print('You typed the name:', name)
+        formatted_langs = [x.strip() for x in request.form['languages'].split(',')]
+        # print('You typed the languages:', formatted_langs)
+        if request.form['birthday']:
+            formatted_date = datetime.strptime(request.form['birthday'], '%Y-%m-%d')
+            # print(formatted_date)
+        else:
+            formatted_date = None
+        return redirect('/characters')
